b5832f802bf7f993b0020df98cb9aa7cb1b9d742
dantin <chengjie.ding@gmail.com>
2019-11-05 22:22:23 +0800

    refactoring config.

--- config.sh
+++ config.sh
@@ -2,7 +2,7 @@
 
 source $(dirname "${BASH_SOURCE[0]}")/set_cloud.sh
 
-ansible-playbook "playbooks/configure_nodes.yml" \
+ansible-playbook "playbooks/config.yml" \
     --inventory="inventory/${cloud_to_use}" \
     --extra-vars="cloud_name=${cloud_to_use}" \
     "$@"
